7b367bcd5d2a00a329a08dd4d8341c48f45e4a9d
davisagli <davisagli@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-05-22 20:35:11 +0000

    workaround to make tests work

--- collective/z3cform/wizard/wizard.py
+++ collective/z3cform/wizard/wizard.py
@@ -125,7 +125,7 @@ class Wizard(utils.OverridableTemplate, form.Form):
         sessionKey = self.sessionKey
         if not self.request.SESSION.has_key(sessionKey):
             self.request.SESSION[sessionKey] = {}
-        if self.request['ACTUAL_URL'] != self.request['HTTP_REFERER']:
+        if self.request['HTTP_REFERER'].startswith('http') and self.request['ACTUAL_URL'] != self.request['HTTP_REFERER']:
             self.request.SESSION[sessionKey] = {}
         self.session = self.request.SESSION[sessionKey]
 
